2ab2574c647228e0b1cb25a2bcf2ee7943a3aac3
Kauan Vidigal <kauan.vidigal@gmail.com>
2025-02-17 20:01:11 -0300

    update

--- sql files/users.sql
+++ sql files/users.sql
@@ -11,7 +11,7 @@
  Target Server Version : 90200
  File Encoding         : 65001
 
- Date: 13/02/2025 19:03:08
+ Date: 17/02/2025 20:01:03
 */
 
 SET NAMES utf8mb4;
@@ -34,8 +34,8 @@ CREATE TABLE `users`  (
   `is_an_admin` tinyint(1) NULL DEFAULT 0,
   `salary` decimal(10, 2) NULL DEFAULT NULL,
   `period` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `work_entry` datetime NULL DEFAULT NULL,
-  `work_exit` datetime NULL DEFAULT NULL,
+  `work_entry` time NULL DEFAULT NULL,
+  `work_exit` time NULL DEFAULT NULL,
   `approved_account` tinyint(1) NULL DEFAULT 0,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
@@ -47,8 +47,8 @@ CREATE TABLE `users`  (
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES (8, 'Kauan Vidigal', 'vidigal@gmail.com', '$2y$12$qG8AgFvVRxRTUSuoGIKxa.SHmQYJjVWK5wPAPS.nfgWAy4nEl/9xi', '015.857.560-16', '15.069.390-4', NULL, 'Dev - Full Stack', 'Apple', 1, 3900.00, 'Vespertino', '2025-02-13 14:00:00', '2025-02-13 19:00:00', 1, '2025-02-12 21:37:51');
-INSERT INTO `users` VALUES (10, 'Fernando Oliveira', 'fernando.oliveira@gmail.com', '$2y$12$LLRt0SqriDl5UiLdXJ4Y1upo6TZ6luW9vWou7OsXibJg9UBWEZV4G', '015.857.560-24', '15.069.390-8', NULL, 'Back - end', 'Apple', 0, 3900.00, 'Vespertino', '2025-02-13 14:00:00', '2025-02-13 19:00:00', 0, '2025-02-13 22:01:10');
-INSERT INTO `users` VALUES (11, 'Maria', 'Maria@gmail.com', '$2y$12$PUaIWZLqtW3zGm/KbXan7OIJ/lxaJePAEw9PR59l0RpFzkWaC19Qq', '015.857.560-50', '15.069.398-4', NULL, 'Dev - Front-end', 'Apple', 0, 2900.00, 'Vespertino', '2025-02-13 14:00:00', '2025-02-13 19:00:00', 0, '2025-02-13 22:02:55');
+INSERT INTO `users` VALUES (8, 'Kauan Vidigal', 'vidigal@gmail.com', '$2y$12$MvwciIgW3C36nq0iSIgxO.F1yy9Rgqw4IV2JLB/SD7asuQdaIXKbq', '015.857.560-16', '15.069.390-4', NULL, 'Dev - Full Stack', 'Apple', 1, 3900.00, 'Vespertino', '14:00:00', '19:00:00', 1, '2025-02-12 21:37:51');
+INSERT INTO `users` VALUES (10, 'Fernando Oliveira', 'fernando.oliveira@gmail.com', '$2y$12$LLRt0SqriDl5UiLdXJ4Y1upo6TZ6luW9vWou7OsXibJg9UBWEZV4G', '015.857.560-24', '15.069.390-8', NULL, 'Back - end', 'Apple', 0, 3900.00, 'Vespertino', '14:00:00', '19:00:00', 0, '2025-02-13 22:01:10');
+INSERT INTO `users` VALUES (11, 'Maria', 'Maria@gmail.com', '$2y$12$PUaIWZLqtW3zGm/KbXan7OIJ/lxaJePAEw9PR59l0RpFzkWaC19Qq', '015.857.560-50', '15.069.398-4', NULL, 'Dev - Front-end', 'Apple', 0, 2900.00, 'Vespertino', '14:00:00', '19:00:00', 0, '2025-02-13 22:02:55');
 
 SET FOREIGN_KEY_CHECKS = 1;
